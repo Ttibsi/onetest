8b0f6dfd674699b5d336d3965a6757713df22eec
ttibsi <ashisbitt@icloud.com>
2025-05-05 21:17:20 +0100

    temp

--- onetest.h
+++ onetest.h
@@ -1,14 +1,23 @@
 #ifndef ONETEST_H
 #define ONETEST_H
 
-#include <stdlib.h> 
-extern int onetest_test_counter;
+#include <stdlib.h>
 
-#define call(func, name) { test_append(func, name); }
+#define call(func, name) { \
+    test_append(func, name); \
+    e = new_errors(); \
+}
 #define ONETEST_STR_LEN 48 
 
-#define assert_eq(x, y) if (x != y) { return 1; } 
-#define assert_ne(x, y) if (x == y) { return 1; } 
+#define assert_eq(x, y) if (x != y) { \
+    error_append(x, "does not equal", y); \
+    return 1;\
+} 
+
+#define assert_ne(x, y) if (x == y) { \
+    error_append(x, "is equal to", y); \
+    return 1;\
+} 
 
 typedef int (*func_ptr)(void); 
 typedef struct {
@@ -17,6 +26,13 @@ typedef struct {
     func_ptr* items;    
     char** names; 
 } test_array;
+
+typedef struct {
+    size_t size;
+    size_t cap;
+    char** items;
+} Errors; 
+extern Errors e; 
 
 void onetest_init(void);
 void onetest_exec(void);
@@ -27,6 +43,7 @@ void onetest_exec(void);
 #include <sys/ioctl.h>
 
 test_array tests; 
+Errors e;
 
 void onetest_init(void) {
     tests.size = 0; 
@@ -47,14 +64,24 @@ void test_append(func_ptr f, char* name) {
     tests.size++; 
 } 
 
+Errors new_errors(void) {
+    if (e.size) { free(e.items); }
+    e.size = 0;
+    e.cap = 8;
+    e.items = malloc(sizeof(char) * ONETEST_STR_LEN * 8); 
+    return e; 
+} 
+
+void error_append() {
+}
+
 size_t get_term_width(void) {
     struct winsize w;
     ioctl(0, TIOCGWINSZ, &w);
     return w.ws_col;
 } 
 
-void onetest_exec(void) { 
-    int failed = 0;
+void onetest_exec(void) {
     const char* fail = "\x1b[41mFailed\x1b[0m";
     const char* succeed = "\x1b[42mPassed\x1b[0m";
 
@@ -70,7 +97,6 @@ void onetest_exec(void) {
         int dest = term_width - 15; 
 
         if (ret) {
-            failed++;
             strcpy(out + dest, fail); 
         } else {
             strcpy(out + dest, succeed); 
