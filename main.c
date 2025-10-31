3eaeea1e623f33ab256be29553f0e8330d8e8a29
Ttibsi <ashisbitt@icloud.com>
2025-10-31 08:56:57 +0000

    update example with skipped test

--- main.c
+++ main.c
@@ -2,20 +2,26 @@
 #include "onetest.h"
 
 int test_foo(void) {
-    onetest_assert_eq(1, 1);
-    onetest_assert_ne(1, 4);
-    return 0; 
+    onetest_assert(1 == 1);
+    onetest_assert(1 != 4);
+    return 0;
 }
 
 int test_bar(void) { 
-    onetest_assert_eq(0.3f, 1.1f);
+    onetest_assert(0.3f == 1.1f);
     return 0; 
+} 
+
+int test_baz(void) {
+    onetest_assert(1 == 2);
+    return 0;
 } 
  
 int main(void) {
     onetest_test_t tests[] = {
         ONETEST_TEST(test_foo),
-        ONETEST_TEST(test_bar)
+        ONETEST_TEST(test_bar),
+        ONETEST_SKIP(test_baz),     
     };
     
     return onetest_exec(tests, sizeof(tests) / sizeof(tests[0]));
